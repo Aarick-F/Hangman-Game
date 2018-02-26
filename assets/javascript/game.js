a183bc92e8033ad01a1aa6dc72c81b32114c331c
Aarick-F <zeflyndesign@gmail.com>
2018-02-25 21:29:31 -0500

    refactored and finished game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,60 +4,84 @@ const words = ["SNACK", "DINNER", "LUNCH", "MORSEL", "CHOMP",
 							"CRUNCH", "LUNCHEON", "MUNCH", "NIBBLE", "BITE",
 							"SALTY", "GNAW", "GNASH", "THRASH", "HUNT"];
 
-// Now, I need to have a word picked from that array of words
-// and have the screen update to show the length of the word in dashes
-// -------------------------------------------------------------------
+let wins = 0,
+		losses = 0;
 
-let thought = getWord(),
+init();
+
+function init() {
+	let picked = "",
 		guesses = 9,
-		wins = 0,
-		losses = 0,
-		guessedLetters = " ";
+		dashes = [],
+		wordArray = [],
+		guessedLetters = "";
+	document.getElementById("displayBoard").innerText = "Oh S#@^! A shark is chasing you! Luckily for you, guessing the word a shark is thinking of paralyzes them! Guess the word!";
+	document.body.style.backgroundColor = "#FFF";
+	document.getElementById("lettersGuessed").innerText = " ";
+	document.getElementById("sprites").style.justifyContent = "space-between";
+	document.getElementById("swimmer").style.opacity = "1";
+	document.getElementById("shark").style.marginLeft = "0px";
+	picked = words[Math.floor(Math.random() * words.length)];
 
-updateBoard(thought, wins, losses, guesses, guessedLetters);
+	for(let i = 0; i < picked.length; i++) {
+		dashes.push("-");
+		wordArray.push(picked[i]);
+	}
+	guesses = 9;
+	document.getElementById("guessesLeft").innerText = guesses;
+	document.getElementById("word").innerText = dashes.join("");
+	console.log(picked);
+	console.log(wordArray);
 
-// Great, now that those are working, time to listen for input 
-// Any time a key is pressed, things in this function will run.
-document.onkeyup = function(event) {
-	let key = event.key.toUpperCase();
-	if(thought.indexOf(key) === -1) {
-		if(guesses > 0) {
-			guesses--;
-			if(guesses <= 6) {
-				document.getElementById("sprites").style.justifyContent = "space-around";
+	let foundLetters = [];
+
+	document.onkeyup = function(event) {
+		let key = event.key.toUpperCase();
+		for(let i = 0; i < wordArray.length; i++) {
+			if(wordArray[i] === key) {
+				foundLetters.push(i);
+				dashes[i] = key;
+				console.log(wordArray);
+				console.log(foundLetters);
+				document.getElementById("word").innerText = dashes.join("");
+				if(foundLetters.length === wordArray.length) {
+					// WIN CONDITION!
+					wins++;
+					document.getElementById("displayBoard").innerText = "Amazing! You stunned the shark and escaped! Click here to play again!";
+					document.getElementById("displayBoard").style.cursor = "pointer";
+					document.getElementById("numWins").innerText = wins;
+					document.body.style.backgroundColor = "#2F2";
+					document.getElementById("displayBoard").addEventListener("click", function() {
+						init();
+					});
+				}
 			}
-			if(guesses <= 3) {
-				document.getElementById("sprites").style.justifyContent = "center";
-			}
-			if(guesses === 0) {
-				document.getElementById("swimmer").style.opacity = "0";
-				document.getElementById("shark").style.marginLeft = "80px";
-				document.body.style.backgroundColor = "#F22";
-				document.getElementById("start").innerText = "Refresh the page to clean up the blood!";
-				updateBoard(thought, wins, losses, guesses, key);
-			}
-			updateBoard(thought, wins, losses, guesses, key);
 		}
+		if(wordArray.indexOf(key) === -1) {
+			document.getElementById("lettersGuessed").innerText += key
+			if(guesses > 0) {
+				guesses--;
+					document.getElementById("guessesLeft").innerText = guesses;
+				if(guesses <= 6) {
+					document.getElementById("sprites").style.justifyContent = "space-around";
+				}
+				if(guesses <= 3) {
+					document.getElementById("sprites").style.justifyContent = "center";
+				}
+				if(guesses === 0) {
+					// LOSE CONDITION!
+					losses++;
+					document.getElementById("displayBoard").innerText = "Oh no! Looks like you got got. Click here to clean up the blood and try again.";
+					document.getElementById("displayBoard").style.cursor = "pointer";
+					document.getElementById("swimmer").style.opacity = "0";
+					document.getElementById("shark").style.marginLeft = "80px";
+					document.body.style.backgroundColor = "#F22";
+					document.getElementById("displayBoard").addEventListener("click", function() {
+						init();
+					});
+				}
+			}
+		}
+		console.log(foundLetters);
 	}
-}
-
-// Function definitions below
-// ----------------------------------------------------------------
-
-function getWord() {
-	let pickedWord = words[Math.floor(Math.random() * words.length)];
-	return pickedWord;
-}
-
-function updateBoard(word, w, l, g, letter) {
-	let hiddenWord = "";
-	for(let i = 0; i < word.length; i++) {
-		hiddenWord += "-";
-	}
-	document.getElementById("word").innerText = hiddenWord;
-	document.getElementById("numWins").innerText = w;
-	document.getElementById("numLosses").innerText = l;
-	document.getElementById("guessesLeft").innerText = g;
-	document.getElementById("lettersGuessed").innerText += letter;
-	console.log(hiddenWord);
 }
