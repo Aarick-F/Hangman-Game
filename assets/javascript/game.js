3d1803c0629f9fd916acdc05981fb8ec418cb411
Aarick-F <zeflyndesign@gmail.com>
2018-02-27 20:18:39 -0500

    refactored code, made it so you cannot guess a duplicate wrong letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,15 @@
 const words = ["SNACK", "DINNER", "LUNCH", "MORSEL", "CHOMP",
 							"TASTE", "CHASE", "DELICIOUS", "SCRUMPTIOUS", 
 							"CRUNCH", "LUNCHEON", "MUNCH", "NIBBLE", "BITE",
-							"SALTY", "GNAW", "GNASH", "THRASH", "HUNT"];
+							"SALTY", "GNAW", "GNASH", "THRASH", "HUNT",
+							"TEETH", "SHRED", "DIVE", "FLOAT", "SPLASH",
+							"APPETITE", "RAGE", "HUNGRY", "STARVING",
+							"FAMISHED", "PECKISH", "BLOOD"];
+
+const winMessage = "Amazing! You stunned the shark and escaped! Click here to play again!";
+const loseMessage = "Oh no! Looks like you got got. Click here to clean up the blood and try again.";
+const blood = "#F22";
+const algae = "#2F2";
 
 let wins = 0,
 		losses = 0;
@@ -14,19 +22,10 @@ function init() {
 			guesses = 9,
 			dashes = [],
 			wordArray = [],
-			guessedLetters = ""
+			guessedLetters = "",
 			game = true;
-
-	document.body.style.backgroundColor = "#FFF";
-	document.getElementById("displayBoard").innerText = "Oh S#@^! A shark is chasing you! Luckily for you, guessing the word a shark is thinking of paralyzes them! Guess the word!";
-	document.getElementById("displayBoard").style.animation = "none";
-	document.getElementById("lettersGuessed").innerText = " ";
-	document.getElementById("sprites").style.justifyContent = "space-between";
-	document.getElementById("swimmer").style.opacity = "1";
-	document.getElementById("swimmer").style.animation = "wave 1.5s linear infinite";
-	document.getElementById("shark").style.animation = "wave 1s linear infinite";
-	document.getElementById("shark").style.marginLeft = "0px";
-	document.getElementById("shark").style.transform = "scaleY(1)";
+	
+	reset();
 	picked = words[Math.floor(Math.random() * words.length)];
 
 	for(let i = 0; i < picked.length; i++) {
@@ -47,25 +46,28 @@ function init() {
 					document.getElementById("word").innerText = dashes.join("");
 					if(dashes.indexOf("-") === -1) {
 						// WIN CONDITION!
-						win();
+						gameOver(true, winMessage, algae);
 					}
 				}
 			}
 			if(wordArray.indexOf(key) === -1 && game === true) {
-				document.getElementById("lettersGuessed").innerText += key
-				if(guesses > 0) {
-					guesses--;
+				let playerGuesses = document.getElementById("lettersGuessed").innerText;
+				if(playerGuesses.indexOf(key) === -1) {
+					document.getElementById("lettersGuessed").innerText += key
+					if(guesses > 0) {
+						guesses--;
 						document.getElementById("guessesLeft").innerText = guesses;
-					if(guesses <= 6) {
-						document.getElementById("sprites").style.justifyContent = "space-around";
-					}
-					if(guesses <= 3) {
-						document.getElementById("sprites").style.justifyContent = "center";
-						document.getElementById("swimmer").style.animation = "wave 0.2s linear infinite";
-					}
-					if(guesses === 0) {
-						// LOSE CONDITION!
-						lose();
+						if(guesses <= 6) {
+							document.getElementById("sprites").style.justifyContent = "space-around";
+						}
+						if(guesses <= 3) {
+							document.getElementById("sprites").style.justifyContent = "center";
+							document.getElementById("swimmer").style.animation = "wave 0.2s linear infinite";
+						}
+						if(guesses === 0) {
+							// LOSE CONDITION!
+							gameOver(false, loseMessage, blood);
+						}
 					}
 				}
 			}
@@ -73,33 +75,36 @@ function init() {
 	}
 }
 
-function win() {
-	wins++;
+function gameOver(end, message, color) {
 	game = false;
-	document.getElementById("displayBoard").innerText = "Amazing! You stunned the shark and escaped! Click here to play again!";
+	if(end) {
+		wins++;
+		document.getElementById("numWins").innerText = wins;
+		document.getElementById("shark").style.animation = "none";
+		document.getElementById("shark").style.transform = "scaleY(-1)";
+		document.getElementById("numWins").innerText = wins;
+	} else {
+		losses++;
+		document.getElementById("swimmer").style.opacity = "0";
+		document.getElementById("shark").style.marginLeft = "80px";
+		document.getElementById("shark").style.animation = "wave 0.1s linear infinite";
+		document.getElementById("numLosses").innerText = losses;
+	}
+	document.body.style.backgroundColor = color;
+	document.getElementById("displayBoard").innerText = message;
 	document.getElementById("displayBoard").style.animation = "wave 1s linear infinite";
-	document.getElementById("shark").style.animation = "none";
-	document.getElementById("shark").style.transform = "scaleY(-1)";
-	document.getElementById("displayBoard").style.cursor = "pointer";
-	document.getElementById("numWins").innerText = wins;
-	document.body.style.backgroundColor = "#2F2";
-	document.getElementById("displayBoard").addEventListener("click", function() {
-		init();
-	});
 }
 
-function lose() {
-	losses++;
-	game = false;
-	document.getElementById("displayBoard").innerText = "Oh no! Looks like you got got. Click here to clean up the blood and try again.";
-	document.getElementById("displayBoard").style.cursor = "pointer";
-	document.getElementById("displayBoard").style.animation = "wave 1s linear infinite";
-	document.getElementById("swimmer").style.opacity = "0";
-	document.getElementById("shark").style.marginLeft = "80px";
-	document.getElementById("shark").style.animation = "wave 0.1s linear infinite";
-	document.body.style.backgroundColor = "#F22";
-	document.getElementById("numLosses").innerText = losses;
-	document.getElementById("displayBoard").addEventListener("click", function() {
-		init();
-	});
+function reset() {
+	document.body.style.backgroundColor = "#FFF";
+	document.getElementById("displayBoard").innerText = "Oh S#@^! A shark is chasing you! Luckily for you, guessing the word a shark is thinking of paralyzes them! Guess the word!";
+	document.getElementById("displayBoard").style.animation = "none";
+	document.getElementById("displayBoard").addEventListener("click", init);
+	document.getElementById("lettersGuessed").innerText = " ";
+	document.getElementById("sprites").style.justifyContent = "space-between";
+	document.getElementById("swimmer").style.opacity = "1";
+	document.getElementById("swimmer").style.animation = "wave 1.5s linear infinite";
+	document.getElementById("shark").style.animation = "wave 1s linear infinite";
+	document.getElementById("shark").style.marginLeft = "0px";
+	document.getElementById("shark").style.transform = "scaleY(1)";
 }
